d6500625a2041e39122dc99838672905380c6eb6
Alfredo Aguirre <thefurok@gmail.com>
2020-12-15 12:08:55 -0500

    iniciamos una aplicacion que sera un web-service

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json('Hola Mundo,probando nodemon');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port http://localhost:3000');
+});
