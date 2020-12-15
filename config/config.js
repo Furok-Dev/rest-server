8b1bf96c5b50137e31116e32b75dbdca04f5196c
Alfredo Aguirre <thefurok@gmail.com>
2020-12-15 12:58:56 -0500

    configuracion de variables de entorno

--- config/config.js
+++ config/config.js
@@ -3,11 +3,11 @@
  * para las variables de entorno
  */
 
-require("dotenv").config();
+require('dotenv').config();
 
 const config = {
-  dev: process.env.NODE_ENV !== "production",
-  port: process.env.PORT || 3000,
+  dev: process.env.NODE_ENV !== 'production',
+  port: process.env.PORT || 5000,
   cors: process.env.CORS,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
