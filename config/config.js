ac6d4a34cf0ee98c3dbb265632111c8f85b4149e
Alfredo Aguirre <thefurok@gmail.com>
2021-01-05 23:04:51 -0500

    configuracion de las variables de entorno

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /**
  * Archivo de configuracion general
  * para las variables de entorno
@@ -9,7 +10,9 @@ const config = {
   dev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || 5000,
   cors: process.env.CORS,
-  dbLocal: process.env.DB_LOCAL,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
 };
 
